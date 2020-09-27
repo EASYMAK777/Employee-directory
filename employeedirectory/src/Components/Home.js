9f46048ec9845224620a984653d6cc14c18ca3c7
EASYMAK777 <jplocallawncare@gmail.com>
2020-09-26 23:40:39 -0400

    added bootstrap cdn

---
+++ employeedirectory/src/Components/Home.js
@@ -0,0 +1,12 @@
+import React, { Container, Row } from "react-bootstrap"
+import React from "react"
+
+export default function Home() {
+    return (
+        <div>
+            <h1>
+                HELLO WORLD!!
+            </h1>
+        </div>
+    )
+}
