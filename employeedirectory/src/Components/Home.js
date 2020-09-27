57db54f63cfd853838446b0d3b9ed9c56348d9ee
EASYMAK777 <jplocallawncare@gmail.com>
2020-09-27 16:46:08 -0400

    added jumbotron to home page

--- employeedirectory/src/Components/Home.js
+++ employeedirectory/src/Components/Home.js
@@ -4,7 +4,10 @@ export default function Home() {
     return (
         <div className ="main-container">
             <div className = "container">
-                
+                <div className = "jumbotron">
+                    <h1>Welcome to the Greed Corp Employee Directory!</h1>
+                </div>
+
             </div>
         </div>
     )
