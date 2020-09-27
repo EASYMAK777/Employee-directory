c4ce0c26ee1a3ee11d2fc5509a55ce990ae6efc1
EASYMAK777 <jplocallawncare@gmail.com>
2020-09-27 17:00:56 -0400

    added h1 h2 to components

--- employeedirectory/src/Components/Login.js
+++ employeedirectory/src/Components/Login.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 export default function Login() {
     return (
-        <div>
-            <h1>LOGIN-PAGE</h1>
+        <div className ="main-container">
+            <div className = "container">
+                <div className = "jumbotron">
+                    <h1>Welcome to the Greed Corp Employee Directory!</h1>
+                    <h2>Please enter Email and Password to use the app0</h2>
+                </div>
+
+            </div>
         </div>
     );
 }
