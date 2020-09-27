bbc5ec682c0362c95e050fd682b1d2dd10ed012f
EASYMAK777 <jplocallawncare@gmail.com>
2020-09-27 18:42:39 -0400

    Finally got Navbar component to work

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,23 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Navbar, Nav, NavDropdown, Form, FormControl, Button, Col } from 'react-bootstrap';
+
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container fluid='xl'>
+          <Row>
+            <Col>
+              <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">Greed Corp Employee Directory</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="mr-auto">
+                    <NavDropdown title="Sort By" id="basic-nav-dropdown">
+                      <NavDropdown.Item href="#action/3.1">By Name</NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.2">By Id</NavDropdown.Item>
+                      <NavDropdown.Divider />
+                   </NavDropdown>
+                  </Nav>
+                  <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-success">Search</Button>
+                  </Form>
+                </Navbar.Collapse>
+              </Navbar>
+            </Col>
+            
+          </Row>
+
+        </Container>
+
+
+
+
+
+
+
       </header>
     </div>
   );
