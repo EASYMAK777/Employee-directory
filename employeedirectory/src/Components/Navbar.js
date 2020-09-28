8370c4fc5cc2c73206858aaddb0cf703dbbcd478
EASYMAK777 <jplocallawncare@gmail.com>
2020-09-27 20:19:37 -0400

    set up navbar component layout

--- employeedirectory/src/Components/Navbar.js
+++ employeedirectory/src/Components/Navbar.js
@@ -1,2 +1,18 @@
 import React from 'react';
-import {Container, Row, Navbar, Nav, NavDropdown, Form, FormControl, Button, Col, Table} from 'react-bootstrap';
+import { Container, Row, Navbar, Nav, NavDropdown, Form, FormControl, Button, Col, Table } from 'react-bootstrap';
+
+function Navbar() {
+    return (
+        <div className="Navbar">
+            <header className = "Navbar-Header">
+                <Container fluid = 'xl'>
+                    <Row>
+                        <Col>
+                        </Col>
+                    </Row>
+                </Container>
+
+            </header>
+        </div>
+    )
+}
