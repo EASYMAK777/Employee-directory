cb5e78f950c7f37f5f4a36bf40e26e6e96ba727f
EASYMAK777 <jplocallawncare@gmail.com>
2020-09-27 20:20:30 -0400

    completed navbar componenet

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -12,23 +12,7 @@ function App() {
         <Container fluid='xl'>
           <Row>
             <Col>
-              <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">Greed Corp Employee Directory</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav className="mr-auto">
-                    <NavDropdown title="Sort By" id="basic-nav-dropdown">
-                      <NavDropdown.Item href="#action/3.1">By Name</NavDropdown.Item>
-                      <NavDropdown.Item href="#action/3.2">By Id</NavDropdown.Item>
-                      <NavDropdown.Divider />
-                    </NavDropdown>
-                  </Nav>
-                  <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-success">Search</Button>
-                  </Form>
-                </Navbar.Collapse>
-              </Navbar>
+              
             </Col>
 
           </Row>
